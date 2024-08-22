f55bd0e3fdb095c7c833de3d2d65eab692e834f7
Je Rome <103312494+hashira09p@users.noreply.github.com>
2024-08-22 03:54:18 +0000

    Done Editing

--- task116.java
+++ task116.java
@@ -1,1 +1,15 @@
+public class task116{
+    public static void main(String[] args) {
+        String input = "Flag";
 
+        StringBuilder palindrome = new StringBuilder("galf");
+        
+        if(input.equals(palindrome)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Input string is not a Palindrome");
+        }
+        
+    }
+}
